291d161093ed259354857fe05bde8647ea1fc014
EBruda <52976547+EBruda@users.noreply.github.com>
2020-01-05 21:01:24 -0500

    Add files via upload

---
+++ Database.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Data.SQLite;
+using System.Windows;
+
+namespace Library
+{
+    class Database
+    {
+        public SQLiteConnection myConnection;
+
+        public Database()
+        {
+            myConnection = new SQLiteConnection("Data Source=database.sqlite3");
+
+            if(!File.Exists("./database.sqlite3"))
+            {
+                MessageBox.Show("Database created");
+                SQLiteConnection.CreateFile("database.sqlite3");
+            }
+        }
+        
+        public void OpenConnection()
+        {
+            if(myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if(myConnection.State != System.Data.ConnectionState.Closed)
+            {
+                myConnection.Close();
+            }
+        }
+    }
+}
---
+++ warning_overdue.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace Library
+{
+    /// <summary>
+    /// Interaction logic for warning_overdue.xaml
+    /// </summary>
+    public partial class warning_overdue : Window
+    {
+        MainWindow MW;
+       
+
+        public warning_overdue(MainWindow MW)
+        {
+            InitializeComponent();
+        }
+
+        private void Return_over_btn_Click(object sender, RoutedEventArgs e)
+        {
+            string id = id_warning.Content.ToString();
+            string isbn = isbn_warning.Content.ToString();
+            string grbc_code = grbc_code_warning.Content.ToString();
+            string title = title_warning.Content.ToString();
+            string author = author_warning.Content.ToString();
+            string holder = holder_warning.Content.ToString();
+            ((MainWindow)Application.Current.MainWindow).Return_book(id, isbn, grbc_code, title, author, holder);
+            
+            this.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void Ignore_btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = System.Windows.Visibility.Hidden;
+        }
+    }
+}
